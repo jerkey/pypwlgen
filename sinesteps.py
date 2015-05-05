42bec1f8a80690873ea62dfb6f8db6818e181b5c
Alex Papazoglou <papazoga@gmail.com>
2015-05-05 11:27:28 -0700

    set NUM_STEPS rather than STEP_TIME

--- sinesteps.py
+++ sinesteps.py
@@ -1,19 +1,20 @@
 # thanks to https://github.com/fwswdev
 import math
 
-TARGET_FILE=r'output.pwl' # why the fuck is there an r after the = sign?
+TARGET_FILE=r'output.pwl' # why the fuck is there an r after the = sign? it means raw string.
 BOOL_DISPLAY_FILE_CONTENTS=True
 TOTAL_TIME=1/60.0
 FREQUENCY=60.0
-STEP_TIME=0.0001
+# STEP_TIME=0.0001
 PEAK_VOLTAGE=240*1.414
-num_steps=int(TOTAL_TIME/STEP_TIME)
-sin_factor=2*math.pi*FREQUENCY*STEP_TIME
-step_time=TOTAL_TIME/num_steps
+NUM_STEPS=256
+step_time=TOTAL_TIME/NUM_STEPS
+sin_factor=2*math.pi*FREQUENCY*step_time
+step_time=TOTAL_TIME/NUM_STEPS
 
 tmp='' # output
 
-for x in range(num_steps):
+for x in range(NUM_STEPS):
     tmp+='%f \t %f\n' % (x * step_time,math.fabs(math.sin(x*sin_factor)*PEAK_VOLTAGE))
 
 with open(TARGET_FILE,'w') as f:
